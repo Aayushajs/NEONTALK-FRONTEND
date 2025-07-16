c7e4f2a10616247ba1db036f60e13d06ba6d58dd
Aayushajs <aayushj004@gmail.com>
2025-07-17 01:13:09 +0530

    some fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://neontalk-backend.onrender.com/api" : "http://localhost:5001/api";
+const BASE_URL ="https://neontalk-backend.onrender.com/api" 
+//const BASE_URL= "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
